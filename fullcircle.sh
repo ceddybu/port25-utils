43a57e5cb996adf0791383a928f0ddbe858c4cf5
Corey Lane <corey.lane@rackspace.com>
2013-12-23 00:31:16 -0600

    moar logic and exception handling

--- fullcircle.sh
+++ fullcircle.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 # define colors
 cyan='\033[1;36m'
@@ -6,16 +7,20 @@ green='\033[1;32m'
 red='\033[1;31m'
 nc='\033[0m'
 
+# check for first argument
 if [ -n "$1" ]
 	then ip=$1; echo -e Checking ${cyan}$ip${nc} for FCrDNS...; echo
-	else ip=`curl -s ipaddr.be`; echo -e $HOSTNAME\'s likely mailing ip is ${cyan}$ip${nc}; echo
+	# curl the public ip
+    else ip=`curl -s ipaddr.be`; echo -e $HOSTNAME\'s likely mailing ip is ${cyan}$ip${nc}; echo
 fi
 
 ptr=`dig +short -x $ip`
-echo -e ${green}OK!${nc} The PTR record for $ip is ${cyan}$ptr${nc}; echo
+if [ -n "$ptr" ]
+    then echo -e ${green}OK!${nc} The PTR record for $ip is ${cyan}$ptr${nc}; echo
+    else echo -e ${red}ptr lookup failed!; exit
+fi
 
 a=`dig +short a $ptr`
-
 if [ $a = $ip ]
 	then echo -e "${green}OK!${nc} $ptr resolves to ${cyan}$a${nc}\n\n${green}FCrDNS established! Hooray!"
 	else echo -e "${red}FCrDNS checks failed!"
