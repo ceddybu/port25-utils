515c77976bcb2ff339114893bfd3a2da67f5bb25
Corey Lane <corey.lane@rackspace.com>
2013-12-22 21:45:02 -0600

    new FCrDNS script

---
+++ fullcircle.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+cyan='\033[1;36m'
+green='\033[1;32m'
+red='\033[1;31m'
+nc='\033[0m'
+
+if [ -n "$1" ]
+	then ip=$1; echo -e Checking ${cyan}$ip${nc} for FCrDNS
+	else ip=`curl -s ipaddr.be`; echo -e $HOSTNAME\'s likely mailing ip: ${cyan}$ip${nc}; echo
+fi
+
+ptr=`dig +short -x $ip`
+echo -e The rDNS record for $ip is ${cyan}$ptr${nc}; echo
+
+arecord=`dig +short a $ptr`
+echo -e $ptr resolves to ${cyan}$arecord${nc}; echo
+
+if [ $arecord = $ip ]
+	then echo -e "${green}FCrDNS established! Hooray!"
+	else echo -e "${red}FCrDNS checks failed!"
+fi
